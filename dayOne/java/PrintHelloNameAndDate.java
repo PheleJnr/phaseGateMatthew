e7eea66d1e00c34f1a1fb94d7b3b74a4e7e611de
PheleJnr <Phelelove96@gmail.com>
2026-08-18 08:18:38 +0100

    dayOne

---
+++ dayOne/java/PrintHelloNameAndDate.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PrintHelloNameAndDate {
+
+    public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter your Name: ");
+        
+        String userName = input.nextLine();
+        
+        int day = 17;
+        int month = 8;
+        int year = 2026;
+        
+        
+    System.out.println("\"My name is " + "\"" + userName + "\"" + "\"");
+    
+    System.out.println("\"Today is " +  day + "/0" + month + "/" + year + "\""); 
+    
+    
+
+    }
+    
+}
+
+
+    
