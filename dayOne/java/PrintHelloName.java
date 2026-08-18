97b36487713ce9475e9e796a3a867885223789f8
PheleJnr <Phelelove96@gmail.com>
2026-08-18 15:17:29 +0100

    Restore dayOne files

---
+++ dayOne/java/PrintHelloName.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PrintHelloName {
+
+    public static void main(String[] args){
+    
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter your Name: ");
+        
+        String userName = input.nextLine();
+            
+    System.out.println("Hello " + userName);
+    
+    
+    
+    }
+    
+}    
---
+++ dayOne/python/print_name_and_date.py
@@ -0,0 +1,15 @@
+name = (input("Enter your name: "))
+
+day = 17
+month = 8
+year = 2026
+
+
+print(f'"My name is "{name}""')
+
+print(f'"Today is {day}/0{month}/{year}"')
+
+
+
+
+
